5ba5ed67c176c16fdfdca2982c6b17a383c1ca00
ramzcarlos <117693942+ramzcarlos@users.noreply.github.com>
2022-11-11 22:50:53 -0600

    Update Main.java

--- Open_ejercicio1/src/proyecto_junit/Main.java
+++ Open_ejercicio1/src/proyecto_junit/Main.java
@@ -4,16 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PizzaOrden pizza = new PizzaOrden();
-		pizza.desplegarMensajeInicio();
-		pizza.desplegarMensajeDefault();
 		
-		
-		
-		pizza.preguntarPizzaVegetariana();
-		pizza.desplegarIngredientes();
-		pizza.guardarIngredientes();
-		pizza.desplegarResumenOrden();
 	}
 
 }
